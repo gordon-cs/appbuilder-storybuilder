566c8a85185f2e427e0b1f21677ed97b09dda666
roddy3 <roddy.ngolomingi@gordon.edu>
2022-01-04 14:49:09 -0500

    Xfade working for circleOpen and fade filter all together

--- main.go
+++ main.go
@@ -100,17 +100,20 @@ func combineVideos(Images []string, Transitions []string, TransitionDurations []
 			check(err)
 			offset += clipDuration + offset - transitionDuration
 			input_images = append(input_images, "-loop", "1", "-ss", Timings[i][0]+"ms", "-t", Timings[i][1]+"ms", "-i", basePath+"/input/"+Images[i])
-			// for some reason the offset for Xfade can not be used for more than 7 second which I do not know yet.
+			//input_filters += fmt.Sprintf(",xfade=transition=circleopen:duration=%sms:offset=%dms", TransitionDurations[i], offset)
+			// input_filters += fmt.Sprintf(",xfade=transition=fade:duration=%sms:offset=%dms", TransitionDurations[i], offset)
+			// input_filters += fmt.Sprintf(",xfade=transition=wipeup:duration=%sms:offset=%dms", TransitionDurations[i], offset)
+			// // for some reason the offset for Xfade can not be used for more than 7 second which I do not know yet.
 			if i == 0 {
 				//input_filters += fmt.Sprintf(",fade=t=out:st=%sms:d=%sms", Timings[i][1], TransitionDurations[i])
-				// println(offset)
+				// 	// println(offset)
+				//input_filters += fmt.Sprintf(",xfade=transition=fade:duration=%sms:offset=%dms", TransitionDurations[i], offset)
 				input_filters += fmt.Sprintf(",xfade=transition=circleopen:duration=%sms:offset=%dms", TransitionDurations[i], offset)
-				// ffmpeg -loop 1 -t 5 -i 1.png -loop 1 -t 5 -i 2.png -filter_complex "[0][1]xfade=transition=fade:duration=1:offset=4.5,format=yuv420p"
 			} else {
 				half_duration, err := strconv.Atoi(TransitionDurations[i])
 				check(err)
 				input_filters += fmt.Sprintf(",fade=t=in:st=0:d=%dms,fade=t=out:st=%sms:d=%dms", half_duration/2, Timings[i][1], half_duration/2)
-				//input_filters += fmt.Sprintf(",xfade=transition=circleopen:duration=%sms:offset=%dms", TransitionDurations[i], offset)
+				//	input_filters += fmt.Sprintf(",xfade=transition=circleopen:duration=%sms:offset=%dms; acrossfade=duration=1 ", TransitionDurations[i], offset)
 			} // write duration and offset and code to multiple by millisecond,
 		}
 		input_filters += fmt.Sprintf("[v%d];", i)
